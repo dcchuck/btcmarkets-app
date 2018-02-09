61b2fc776c6bc61e9308bf6c988e41de34001a25
dcchuck <chuck.danielsson@gmail.com>
2018-02-08 21:43:53 -0500

    build it right

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
 	output: {
-		path: path.join(__dirname, 'public', 'js'),
+		path: path.join(__dirname, 'public'),
 		filename: 'index.js'
 	},
 	devServer: {
 		contentBase: path.join(__dirname, 'public'),
 		compress: true,
 		port: 3000
-	}
+	},
+	plugins: [
+		new CopyWebpackPlugin(['static'])
+	]
 }
