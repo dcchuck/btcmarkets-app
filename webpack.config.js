0c64a01747fb32aeb2c11d7c6afce0e90c8e513a
dcchuck <chuck.danielsson@gmail.com>
2018-02-08 21:04:11 -0500

    dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, 'public', 'js'),
-        filename: 'index.js'
-    }
+	entry: './src/index.js',
+	output: {
+		path: path.join(__dirname, 'public', 'js'),
+		filename: 'index.js'
+	},
+	devServer: {
+		contentBase: path.join(__dirname, 'public'),
+		compress: true,
+		port: 3000
+	}
 }
